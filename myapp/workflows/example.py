dd0f09f8c5ba1ef4a9494019e62b4a1e33af89e2
cosmicBboy <niels.bantilan@gmail.com>
2021-04-15 14:16:08 -0400

    revert example.py changes
    Signed-off-by: cosmicBboy <niels.bantilan@gmail.com>

--- myapp/workflows/example.py
+++ myapp/workflows/example.py
@@ -2,11 +2,11 @@ from flytekit import task, workflow
 
 
 @task
-def greet(first_name: str, last_name: str) -> str:
+def greet(name: str) -> str:
     return f"Hello, {first_name} {last_name}"
 
 
 @workflow
-def hello_world(first_name: str = "world", last_name: str = "earth") -> str:
-    greeting = greet(first_name=first_name, last_name=last_name)
+def hello_world(name: str = "world") -> str:
+    greeting = greet(name=name)
     return greeting
