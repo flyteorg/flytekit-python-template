3c42488268dd15f418f01c3706aeebb8b35c6ab5
Ketan Umare <ketan.umare@gmail.com>
2021-06-23 10:19:42 -0700

    Improved docker_build script
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>

--- docker_build_and_tag.sh
+++ docker_build_and_tag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 # SET the REGISTRY here, where the docker container should be pushed
 REGISTRY=""
@@ -8,15 +8,33 @@ REGISTRY=""
 # SET the appname here
 APP_NAME="myapp"
 
+while getopts a:r:v:h flag
+do
+    case "${flag}" in
+        a) APP_NAME=${OPTARG};;
+        r) REGISTRY=${OPTARG};;
+        v) VERSION=${OPTARG};;
+        h) echo "Usage: ${0} [-h|[-a <app_name>][-r <registry_name>][-v <version>]]"
+           echo "  h: help (this message)"
+           echo "  a: APP_NAME or the REPOSITORY APP_NAME. Defaults to myapp."
+           echo "  r = REGISTRY name where the docker container should be pushed. Defaults to none - localhost"
+           echo "  v = VERSION of the build. Defaults to using the current git head SHA"
+           exit 0
+    esac
+done
+
 # If you are using git, then this will automatically use the git head as the
 # version
-VERSION=$(git rev-parse HEAD)
+if [ -z "${VERSION}" ]; then
+  echo "No version set, using git commit head sha as the version"
+  VERSION=$(git rev-parse HEAD)
+fi
 
 TAG=${APP_NAME}:${VERSION}
 if [ -z "${REGISTRY}" ]; then
   echo "No registry set, creating tag ${TAG}"
 else
- TAG="${REGISTRY_NAME}/${TAG}"
+ TAG="${REGISTRY}/${TAG}"
  echo "Registry set: creating tag ${TAG}"
 fi
 
