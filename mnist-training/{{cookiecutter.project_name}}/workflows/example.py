0b4d806d00ec1864236fc667338ae549e1ee1d1c
zeryx <1892175+zeryx@users.noreply.github.com>
2023-03-22 16:37:21 -0400

    added doc strings, and added dependency version pegging
    Signed-off-by: zeryx <1892175+zeryx@users.noreply.github.com>

--- mnist-training/{{cookiecutter.project_name}}/workflows/example.py
+++ mnist-training/{{cookiecutter.project_name}}/workflows/example.py
@@ -4,16 +4,43 @@ from torchvision import datasets, transforms
 from flytekit import task, dynamic, Resources
 import torch as th
 from torch import nn
-
+"""
+This example is a simple MNIST training example. It uses the PyTorch framework to train a simple CNN model on the MNIST dataset.
+The model is trained for 10 epochs and the validation loss is calculated on the test set.
+"""
 @task
 def get_dataset(training: bool, gpu: bool = False) -> DataLoader:
-    print("GPU Enabled: " + str(gpu))
+    """
+    This task downloads the MNIST dataset and returns a dataloader for the dataset.
+    If GPU is enabled, the dataloader is configured to use the GPU.
+    :return: A dataloader for the MNIST dataset.
+    """
     dataset = datasets.MNIST("/tmp/mnist", train=training, download=True, transform=transforms.ToTensor())
     if gpu:
         dataloader = DataLoader(dataset, batch_size=64, shuffle=True, pin_memory_device="cuda", pin_memory=True)
     else:
         dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
     return dataloader
+
+@task(requests=Resources(cpu="2", mem="10Gi"))
+def train_model_cpu(model: th.nn.Sequential, optim: th.optim.Optimizer,  dataset: DataLoader, n_epochs: int) -> th.nn.Sequential:
+    """
+    This task trains the model for the specified number of epochs.
+    This variant of the task uses the CPU for training. as you can see from the Resources requested in the task decorator.
+    """
+    return train_model(model=model, optim=optim, dataset=dataset, n_epochs=n_epochs)
+
+@task(requests=Resources(gpu="1", mem="10Gi"))
+def train_model_gpu(model: th.nn.Sequential, optim: th.optim.Optimizer,  dataset: DataLoader, n_epochs: int) -> th.nn.Sequential:
+    """
+    This task trains the model for the specified number of epochs.
+    This variant of the task uses the GPU for training. as you can see from the Resources requested in the task decorator.
+    """
+    return train_model(model=model, optim=optim, dataset=dataset, n_epochs=n_epochs)
+
+"""
+General Functions, used by Tasks
+"""
 
 
 def get_model_architecture() -> (th.nn.Sequential, th.optim.Optimizer):
@@ -32,16 +59,12 @@ def get_model_architecture() -> (th.nn.Sequential, th.optim.Optimizer):
     optimizer = th.optim.SGD(model.parameters(), lr=0.003, momentum=0.9)
     return model, optimizer
 
-@task(requests=Resources(cpu="2", mem="10Gi"))
-def train_model_cpu(model: th.nn.Sequential, optim: th.optim.Optimizer,  dataset: DataLoader, n_epochs: int) -> th.nn.Sequential:
-    return train_model(model=model, optim=optim, dataset=dataset, n_epochs=n_epochs)
-
-@task(requests=Resources(gpu="1", mem="10Gi"))
-def train_model_gpu(model: th.nn.Sequential, optim: th.optim.Optimizer,  dataset: DataLoader, n_epochs: int) -> th.nn.Sequential:
-    return train_model(model=model, optim=optim, dataset=dataset, n_epochs=n_epochs)
-
 
 def train_model(model: th.nn.Sequential, optim: th.optim.Optimizer,  dataset: DataLoader, n_epochs: int) -> th.nn.Sequential:
+    """
+    This function runs the inner training loop for the specified number of epochs.
+    If a GPU is available, the model is moved to the GPU and the training is done on the GPU.
+    """
     if th.cuda.is_available():
         model.to("cuda").train()
     else:
@@ -59,6 +82,10 @@ def train_model(model: th.nn.Sequential, optim: th.optim.Optimizer,  dataset: Da
 
 @task(requests=Resources(cpu="2", mem="10Gi"))
 def validation_loss(model: th.nn.Sequential, dataset: DataLoader) -> str:
+    """
+    This task calculates the validation loss on the test set.
+    This is always run in CPU mode, regardless of the GPU setting. It simply returns the NNL Model Loss on the test set.
+    """
     model.to("cpu").eval()
     losses = []
     with torch.no_grad():
@@ -76,7 +103,14 @@ def validation_loss(model: th.nn.Sequential, dataset: DataLoader) -> str:
 
 
 @dynamic
-def train_mnist_model(n_epoch: int = 10, gpu_enabled: bool =False) -> str:
+def mnist_workflow(n_epoch: int = 10, gpu_enabled: bool =False) -> str:
+    """Declare workflow called `wf`.
+    The @dynamic decorator defines a dynamic workflow.
+    Dynamic workflows allow for executing arbitrary python code, and are useful for cases where the
+    workflow is not known at compile time.
+
+    This particular workflow is dynamic to enable the user to choose whether to run the training on the GPU or not.
+    """
     training_dataset = get_dataset(training=True, gpu=gpu_enabled)
     test_dataset = get_dataset(training=False, gpu=gpu_enabled)
     model, optim = get_model_architecture()
@@ -86,3 +120,9 @@ def train_mnist_model(n_epoch: int = 10, gpu_enabled: bool =False) -> str:
         trained_model = train_model_cpu(model=model, optim=optim, dataset=training_dataset, n_epochs=n_epoch)
     output = validation_loss(model=trained_model, dataset=test_dataset)
     return output
+
+
+if __name__ == "__main__":
+    # Execute the workflow, simply by invoking it like a function and passing in
+    # the necessary parameters
+    print(f"Running wf() { mnist_workflow(n_epoch=10, gpu_enabled=False) }")
