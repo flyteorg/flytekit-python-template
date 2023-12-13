66447e8345822b9f21d58ee104965189954a510e
nikki everett <nikki@union.ai>
2023-12-13 09:07:47 -0600

    use cookiecutter for hello world template
    Signed-off-by: nikki everett <nikki@union.ai>

--- hello-world/{{cookiecutter.project_name}}/example.py
+++ hello-world/{{cookiecutter.project_name}}/example.py
@@ -11,7 +11,7 @@ def say_hello(name: str) -> str:
 @workflow
 def hello_world_wf(name: str = 'world') -> str:
     res = say_hello(name=name)
-    return res 
+    return res
 
 
 if __name__ == "__main__":
