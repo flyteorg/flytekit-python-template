57017067387e0b2e2f6addb702a8df9483c39156
nikki everett <nikki@union.ai>
2024-01-02 13:41:31 -0600

    update comments
    Signed-off-by: nikki everett <nikki@union.ai>

--- basic-template-imagespec/{{cookiecuter.project_name}}/workflows/example.py
+++ basic-template-imagespec/{{cookiecuter.project_name}}/workflows/example.py
@@ -21,12 +21,12 @@ For more information, see the
 
 # image_definition = ImageSpec(
 #    name="flytekit",  # rename this to your docker image name
-#    base_image="ghcr.io/flyteorg/flytekit:py3.11-1.10.2",
 #    # the base image that flytekit will use to build your image
+#    base_image="ghcr.io/flyteorg/flytekit:py3.11-1.10.2",
 #    packages=["flytekit>=1.6.0"],  # packages to add to the base image
 #    registry="ghcr.io/unionai-oss", # registry your image will be pushed to
-#    python_version="3.11"
 #    # the python version; optional if not different from the base image
+#    python_version="3.11"
 # )
 
 
