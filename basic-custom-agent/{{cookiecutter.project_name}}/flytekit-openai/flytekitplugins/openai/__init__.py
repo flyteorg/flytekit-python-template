26af7613aa0da53b62e68ccab5b0917f116fd70b
Vincent Chen <62143443+mao3267@users.noreply.github.com>
2025-01-17 22:43:01 +0800

    [Flytekit] Add custom agent template in Pyflyte Init (#51)
    * feat: add template for custom agent build
    
    Signed-off-by: mao3267 <chenvincent610@gmail.com>
    
    * fix: directory tree
    
    Signed-off-by: mao3267 <chenvincent610@gmail.com>
    
    * fix: update sync agent openai
    
    Signed-off-by: mao3267 <chenvincent610@gmail.com>
    
    * fix: rename directory
    
    Signed-off-by: mao3267 <chenvincent610@gmail.com>
    
    * fix: update plugin install source
    
    Signed-off-by: mao3267 <chenvincent610@gmail.com>
    
    * fix: copy cmd error
    
    Signed-off-by: mao3267 <chenvincent610@gmail.com>
    
    * fix: use template from Future-Outlier/flyte-custom-agent-template
    
    Signed-off-by: mao3267 <chenvincent610@gmail.com>
    
    * fix: standardize getopts parameters
    
    Signed-off-by: mao3267 <chenvincent610@gmail.com>
    
    * fix: add newline
    
    Signed-off-by: mao3267 <chenvincent610@gmail.com>
    
    ---------
    
    Signed-off-by: mao3267 <chenvincent610@gmail.com>

---
+++ basic-custom-agent/{{cookiecutter.project_name}}/flytekit-openai/flytekitplugins/openai/__init__.py
@@ -0,0 +1,16 @@
+"""
+.. currentmodule:: flytekitplugins.openai
+
+.. autosummary::
+   :template: custom.rst
+   :toctree: generated/
+
+   DownloadJSONFilesTask
+   UploadJSONLFileTask
+   OpenAIFileConfig
+   ChatGPTAgent
+   ChatGPTTask
+"""
+
+from .chatgpt.agent import ChatGPTAgent
+from .chatgpt.task import ChatGPTTask
