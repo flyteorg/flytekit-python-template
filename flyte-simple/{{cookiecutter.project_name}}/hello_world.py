bdd4c20ffa2ac54bab1e494692e65b0f4edaf49a
Peeter Piegaze <1153481+ppiegaze@users.noreply.github.com>
2025-03-06 18:02:48 +0100

    Add new template for use in Flyte docs

---
+++ flyte-simple/{{cookiecutter.project_name}}/hello_world.py
@@ -0,0 +1,24 @@
+"""Hello World"""
+
+import union
+
+image_spec = union.ImageSpec(
+
+    # Build the image using Union's built-in cloud builder (not locally on your machine)
+    builder="union",
+
+    # The name of the image. This image will be used byt he say_hello task
+    name="say-hello-image",
+
+    # Lock file with dependencies to install in image
+    requirements="uv.lock",
+)
+
+@union.task(container_image=image_spec)
+def say_hello(name: str) -> str:
+    return f"Hello, {name}!"
+
+@union.workflow
+def hello_world_wf(name: str = "world") -> str:
+    greeting = say_hello(name=name)
+    return greeting
